13b779ec6a39b9ecc380dc2887bfd4b8b1b1edc6
01Gibbs <danielgilbert49@hotmail.co.uk>
2023-02-24 15:15:41 +0000

    09-GET_/api/users refactor per comments

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,8 +1,7 @@
 const { fetchUsers } = require('../models/user-model')
 
 const getUsers = (request, response, next) => {
-  const userId = request.params.user_id
-  fetchUsers(userId).then((users) => {
+  fetchUsers().then((users) => {
     response.status(200).send({ users })
   })
 }
--- models/user-model.js
+++ models/user-model.js
@@ -1,25 +1,12 @@
 const db = require('../db/connection.js')
 
 const fetchUsers = (userId) => {
-  let queryString = `SELECT * FROM users`
-  const queryParams = []
+  const queryString = `SELECT * FROM users`
 
-  if (userId !== undefined) {
-    queryString += `WHERE user_id = $1`
-    queryParams.push(userId)
-  }
-
-  return db.query(queryString, queryParams).then((result) => {
+  return db.query(queryString).then((result) => {
     const rowCount = result.rowCount
-    if (rowCount < 1) {
-      return Promise.reject({ status: 404, msg: 'user not found' })
-    } else {
-      if (rowCount === 1) {
-        return result.rows[0]
-      } else {
-        return result.rows
-      }
-    }
+
+    return result.rows
   })
 }
 
